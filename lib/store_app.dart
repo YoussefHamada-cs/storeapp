856935793de88890203fedbf8755def7b76ca9c6
youssef hamada <yosefhagag84171@gmail.com>
2025-05-06 00:44:03 +0300

    start app

---
+++ lib/store_app.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo',
+    home: Scaffold(),
+    );
+  }
+}
