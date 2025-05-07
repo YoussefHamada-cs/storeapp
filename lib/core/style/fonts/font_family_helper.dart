2126adfb2611d648696cac0c55a86a52f3681914
youssef hamada <yosefhagag84171@gmail.com>
2025-05-08 01:29:49 +0300

    setup fonts

---
+++ lib/core/style/fonts/font_family_helper.dart
@@ -0,0 +1,4 @@
+class FontFamilyHelper {
+  static String cairo = 'Cairo';
+  static String poppins = 'Poppins';
+}
---
+++ lib/core/style/fonts/font_weight_helper.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class FontWeightHelper {
+  static FontWeight bold = FontWeight.w700;
+  static FontWeight medium = FontWeight.w500;
+  static FontWeight regular = FontWeight.w400;
+ 
+}
