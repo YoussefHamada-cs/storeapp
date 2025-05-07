a2fa2eb4009a88933025c6ad38ada2368f69068c
youssef hamada <yosefhagag84171@gmail.com>
2025-05-07 23:25:28 +0300

    flutter_screenutil done

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:storeapp/core/app/connactivity_controller.dart';
 import 'package:storeapp/core/views/no_network_view.dart';
 
@@ -12,20 +13,24 @@ class StoreApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isOnline,
       builder: (context, value, child) {
         if (value) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Store',
-            builder: (context, child) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectivityController.instance.init();
-                    return child!;
-                  },
-                ),
-              );
-            },
-            home: Scaffold(appBar: AppBar(title: Text('storeapp'))),
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Store',
+              builder: (context, child) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return child!;
+                    },
+                  ),
+                );
+              },
+              home: Scaffold(appBar: AppBar(title: Text('storeapp'))),
+            ),
           );
         } else {
           return MaterialApp(home: NoNetworkView());
