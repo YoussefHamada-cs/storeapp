86aebc8efa22078310d8bf9d2c794c4fa8261472
youssef hamada <yosefhagag84171@gmail.com>
2025-05-15 18:19:37 +0300

    dio fectory

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:storeapp/core/app/app_cubit/app_cubit.dart';
+import 'package:storeapp/core/service/graphql/api_service.dart';
+import 'package:storeapp/core/service/graphql/dio_factory.dart';
 
 final sl = GetIt.instance;
 Future<void> setupInjector() async {
@@ -7,5 +9,7 @@ Future<void> setupInjector() async {
 }
 
 Future<void> _initCore() async {
+  final dio =  DioFactory.getDio();
   sl.registerFactory(() => AppCubit());
+  sl.registerLazySingleton<ApiService>(() => ApiService(dio));
 }
